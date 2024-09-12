a77e30e2ac9417317ae54afc3091058cdb3a8a8f
Biplov Kumar <Kumarb@thepaycocld.onmicrosoft.com>
2024-09-12 11:14:24 +0530

    Added Configuration

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,12 @@
 import firebase from '@react-native-firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlYqvkBNrBqxR-bsdGNS4JLUKU6pR0HyY",
-  authDomain: "flavour-ba610.firebaseapp.com",
-  projectId: "flavour-ba610",
-  storageBucket: "flavour-ba610.appspot.com",
-  messagingSenderId: "1079342389440",
-  appId: "1:1079342389440:web:a027234653ec54fbaa7525"
+  apiKey: 'AIzaSyBlYqvkBNrBqxR-bsdGNS4JLUKU6pR0HyY',
+  authDomain: 'flavour-ba610.firebaseapp.com',
+  projectId: 'flavour-ba610',
+  storageBucket: 'flavour-ba610.appspot.com',
+  messagingSenderId: '1079342389440',
+  appId: '1:1079342389440:web:a027234653ec54fbaa7525',
 };
 
 // if (!firebase.apps.length) {
@@ -15,8 +15,9 @@ const firebaseConfig = {
 
 let app;
 if (firebase.apps.length === 0) {
-  app = firebase.initializeApp(firebaseConfig)
+  app = firebase.initializeApp(firebaseConfig);
 } else {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   app = firebase.app();
 }
 
